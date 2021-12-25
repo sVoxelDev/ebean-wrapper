8c665e9e1155baab4461698f2c4d57ef0da4baa8
SgtJalau <fofololkok@googlemail.com>
2021-12-25 20:33:19 +0100

    Added support for BungeeCord

---
+++ src/main/java/net/silthus/ebean/EbeanWrapperBungee.java
@@ -0,0 +1,13 @@
+package net.silthus.ebean;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+/**
+ * Created by Maurice on 04.01.2017.
+ */
+public class EbeanWrapperBungee extends Plugin {
+
+    @Override
+    public void onLoad() {
+    }
+}
