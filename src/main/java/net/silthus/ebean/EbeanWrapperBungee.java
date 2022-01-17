1faa7fac9575b3111d1f137e0fc5072bf62c6c35
SgtJalau <fofololkok@googlemail.com>
2022-01-17 16:38:52 +0100

    feat: bungee support & fixed migrations running without data set

--- src/main/java/net/silthus/ebean/EbeanWrapperBungee.java
+++ src/main/java/net/silthus/ebean/EbeanWrapperBungee.java
@@ -2,9 +2,6 @@ package net.silthus.ebean;
 
 import net.md_5.bungee.api.plugin.Plugin;
 
-/**
- * Created by Maurice on 04.01.2017.
- */
 public class EbeanWrapperBungee extends Plugin {
 
     @Override
